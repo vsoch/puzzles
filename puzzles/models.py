055dc04dc5d068699ba4a49ff19a5b2d3fd37b0c
vsoch <vsoch@users.noreply.github.com>
2021-06-20 22:11:13 -0600

    start of attempt to refactor
    the solver is 20 seconds slower and does not seem to be working
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- puzzles/models.py
+++ puzzles/models.py
@@ -16,14 +16,21 @@ import os
 import sys
 
 
-class PlacedPiece:
-    def __init__(self, index, loc):
-
-        # The index of the piece in the puzzle array
+class PuzzlePiece:
+    def __init__(self, data, index, loc):
         self.index = index
+        self.data = data
 
         # The x,y coordinate, not pixel location but piece location
         self.loc = loc
+
+    @property
+    def x(self):
+        return self.loc[0]
+
+    @property
+    def y(self):
+        return self.loc[1]
 
     def __str__(self):
         return "PuzzlePiece(index=%s, loc=%s)" % (self.index, self.loc)
@@ -63,16 +70,18 @@ class PhotoPuzzle:
         """
         Given a loaded puzzle image, generate some number of pieces.
         """
+        count = 0
         for i in range(self.horizontal_num_pieces):
             for j in range(self.vertical_num_pieces):
                 # Split into pieces with dimensions x, y, and color
-                self.pieces.append(
+                self.pieces.append(PuzzlePiece(
                     self.image[
                         j * self.piece_height : (j + 1) * self.piece_height,
                         i * self.piece_width : (i + 1) * self.piece_width,
                         :,
-                    ]
+                    ], count, (i, j))
                 )
+                count +=1
 
     def __exit__(self):
         """
@@ -120,7 +129,19 @@ class PhotoPuzzle:
         """
         Shuffle the pieces, and reset open and covered pieces.
         """
+        # Shuffle locations
+        locs = [(p.x, p.y) for p in self.pieces]
+        np.random.shuffle(locs)
+
+        # And reassign
+        for i, piece in enumerate(self.pieces):
+            piece.loc = locs[i]
+
         np.random.shuffle(self.pieces)
+
+        # Updates to new indices so we can lookup data 
+        for i, piece in enumerate(self.pieces):
+            piece.index = i
         self.open_places = collections.defaultdict(set)
         self.covered_places = {}
 
@@ -153,47 +174,46 @@ class PhotoPuzzle:
         Given unused piece indices, calculate
         """
         scores = {}
-        for new_piece_index in unused_pieces_indices:
+        for piece_index in unused_pieces_indices:
             for loc, placed_neighbors in self.open_places.items():
-                scores[new_piece_index, loc] = np.mean(
+                new_piece = self.pieces[piece_index]
+                scores[piece_index, loc] = np.mean(
                     [
-                        self.find_score(
-                            new_piece=PlacedPiece(index=new_piece_index, loc=loc),
-                            neighbor_piece=neighbor_piece,
-                        )
+                        self.find_score(loc, new_piece.index, neighbor_piece=neighbor_piece)
                         for neighbor_piece in placed_neighbors
                     ]
                 )
         return scores
 
-    def get_piece_distance(self, p1, e1, p2, e2):
+    def get_piece_distance(self, index1, edge_index1, index2, edge_index2):
         """
         Calculate and cache the distance between two pieces
         """
-        if (p1, e1, p2, e2) not in self.piece_distances:
-            edge1 = self.get_piece_edges(p1, e1)
-            edge2 = self.get_piece_edges(p2, e2)
-            self.piece_distances[p1, e1, p2, e2] = self.edge_matching_score(
+        key = (index1, edge_index1, index2, edge_index2) 
+        if key not in self.piece_distances:
+            edge1 = self.get_piece_edges(index1, edge_index1)
+            edge2 = self.get_piece_edges(index2, edge_index2)
+            self.piece_distances[key] = self.edge_matching_score(
                 edge1, edge2
             )
-        return self.piece_distances[p1, e1, p2, e2]
+        return self.piece_distances[key]
 
-    def get_piece_edges(self, p, e):
+    def get_piece_edges(self, index, edge_index):
         """
         Get edges of a piece.
 
         This is where we link data from self.pieces into our calculation
         """
-        piece = self.pieces[p]
-        if e == 0:
+        piece = self.pieces[index].data
+        if edge_index == 0:
             return piece[0, :]
-        if e == 1:
+        if edge_index == 1:
             return piece[:, -1]
-        if e == 2:
+        if edge_index == 2:
             return piece[-1, :][::-1]
-        if e == 3:
+        if edge_index == 3:
             return piece[:, 0][::-1]
-        raise ValueError("e should be 0, 1, 2, or 3. Got %s." % e)
+        raise ValueError("e should be 0, 1, 2, or 3. Got %s." % edge_index)
 
     def edge_matching_score(self, edge1, edge2):
         """
@@ -204,28 +224,29 @@ class PhotoPuzzle:
         edge_diff = edge1 - edge2[::-1]
         return np.log(la.norm(np.minimum(edge_diff, self.max_rgb - edge_diff), ord=1))
 
-    def find_score(self, new_piece, neighbor_piece):
+    def find_score(self, loc, index, neighbor_piece):
+
         """
         Calculate a score between two pieces
         """
-        x_diff = new_piece.loc[0] - neighbor_piece.loc[0]
-        y_diff = new_piece.loc[1] - neighbor_piece.loc[1]
+        x_diff = loc[0] - neighbor_piece.loc[0]
+        y_diff = loc[1] - neighbor_piece.loc[1]
 
         # New piece to the RIGHT of neighbor
         if x_diff == 1 and y_diff == 0:
-            return self.get_piece_distance(new_piece.index, 3, neighbor_piece.index, 1)
+            return self.get_piece_distance(index, 3, neighbor_piece.index, 1)
 
         # New piece to the LEFT of neighbor
         if x_diff == -1 and y_diff == 0:
-            return self.get_piece_distance(new_piece.index, 1, neighbor_piece.index, 3)
+            return self.get_piece_distance(index, 1, neighbor_piece.index, 3)
 
         # New piece BELOW neighbor
         if x_diff == 0 and y_diff == 1:
-            return self.get_piece_distance(new_piece.index, 0, neighbor_piece.index, 2)
+            return self.get_piece_distance(index, 0, neighbor_piece.index, 2)
 
         # New piece ABOVE neighbor
         if x_diff == 0 and y_diff == -1:
-            return self.get_piece_distance(new_piece.index, 2, neighbor_piece.index, 0)
+            return self.get_piece_distance(index, 2, neighbor_piece.index, 0)
 
         raise ValueError(
             f"Found x_diff={x_diff} and y_diff={y_diff} between "
@@ -242,10 +263,11 @@ class PhotoPuzzle:
         self.covered_places = {}
 
         # Add the first piece.
-        self.add_piece(PlacedPiece(index=0, loc=(0, 0)))
+        piece = self.pieces[0]
+        self.add_piece(piece)
 
         # Get indices of unused pieces
-        unused_pieces_indices = set(range(len(self.pieces))) - {0}
+        unused_pieces_indices = set(range(len(self.pieces))) - {piece.index}
 
         # Add remaining pieces to the puzzle
         while unused_pieces_indices:
@@ -254,7 +276,8 @@ class PhotoPuzzle:
             scores = self.make_scores(unused_pieces_indices)
             new_index, new_loc = min(scores, key=scores.get)
             unused_pieces_indices.remove(new_index)
-            self.add_piece(PlacedPiece(index=new_index, loc=new_loc))
+            chosen_piece = self.pieces[new_index]
+            self.add_piece(chosen_piece)
 
         self.reset_covered_indices()
 
@@ -266,20 +289,27 @@ class PhotoPuzzle:
             for (x, y), index in self.covered_places.items()
         }
 
-    def plot_puzzle(self, n_rows, n_cols, plot_piece):
+    def plot(self):
         """
         Shared function to plot puzzle
         """
+        # Figure out number of rows and columns we need
+        x_min = min([p.loc[0] for p in self.pieces])
+        y_min = min([p.loc[1] for p in self.pieces])
+        x_max = max([p.loc[0] for p in self.pieces])
+        y_max = max([p.loc[1] for p in self.pieces])
+        n_rows = y_max - y_min + 1
+        n_cols = x_max - x_min + 1
+
         fig, axs = plt.subplots(n_rows, n_cols)
         fig.set_figheight(n_rows)
         fig.set_figwidth(n_cols)
 
-        for y, ax_row in enumerate(axs):
-            for x, ax in enumerate(ax_row):
-                plot_piece(ax, x, y)
-                ax = axs[y, x]
-                ax.xaxis.set_visible(False)
-                ax.axes.yaxis.set_visible(False)
+        for piece in self.pieces:            
+            ax = axs[piece.y, piece.x]
+            ax.xaxis.set_visible(False)
+            ax.axes.yaxis.set_visible(False)
+            ax.imshow(piece.data)
 
         for col, ax in enumerate(axs[0]):
             ax.set_title(col)
@@ -289,39 +319,6 @@ class PhotoPuzzle:
             ax.axes.yaxis.set_visible(True)
             ax.axes.yaxis.set_ticks([])
         return fig
-
-    def get_puzzle_figure(self):
-        """
-        Show the current state of the puzzle.
-
-        This function plots self.pieces, not the puzzle solution.
-        """
-
-        def plot_piece(ax, x, y):
-            ax.imshow(self.pieces[y + x * self.vertical_num_pieces])
-
-        return self.plot_puzzle(
-            self.vertical_num_pieces, self.horizontal_num_pieces, plot_piece
-        )
-
-    def get_solved_figure(self):
-        """
-        Get figure for solved puzzle
-        """
-        n_rows = max([loc[1] for loc in self.covered_places]) + 1
-        n_cols = max([loc[0] for loc in self.covered_places]) + 1
-
-        # The solved figure has a different function to plot the piece
-        def plot_covered_piece(ax, x, y):
-            if (x, y) in self.covered_places:
-                piece_index = self.covered_places[(x, y)]
-                ax.imshow(self.pieces[piece_index])
-
-        return self.plot_puzzle(
-            n_rows=max([loc[1] for loc in self.covered_places]) + 1,
-            n_cols=max([loc[0] for loc in self.covered_places]) + 1,
-            plot_piece=plot_covered_piece,
-        )
 
     @property
     def vertical_num_pieces(self):
